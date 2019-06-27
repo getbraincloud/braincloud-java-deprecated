bb2fb7b8be46eb253a1a012b7aa5fe5c7910f89f
ruth0048 <31827673+ruth0048@users.noreply.github.com>
2019-06-27 17:04:10 -0400

    Release 4.1.0 (#55) (#56)
    * adding register and deregister RTTPresence callback
    
    * Inconsistent wrapper and comms initialize function calls. Improper param placements cause problems with comms
    
    * Fixing parameters in certain calls
    
    * quick fix to chat test
    
    * Bug fix for overly aggressive rest client thread polling.
    
    * Re-addition of InitializeWithApps, also added overload
    
    *  adding a note to keep initializeWithApps in the java client tests
    
    * 4.0 Api calls, and test fix
    
    * fixing java social leaderbaord tests.
    
    * RTT service java
    
    * cancelLobby
    
    * Need to take a second look at this assert
    
    * guard for sessions and profile id save
    
    * Updating version for release
    
    * Bad sig fix and tests to java
    
    * GroupLeaderboard calls and tests
    
    * changing groupLeaderboard Config again
    
    * group calls and tests java
    
    * changing createGroup to createGroupWithSummaryData for consistency across clients
    
    * updating version for java

--- brainCloud/test/java/com/bitheads/braincloud/services/AuthenticationServiceTest.java
+++ brainCloud/test/java/com/bitheads/braincloud/services/AuthenticationServiceTest.java
@@ -6,6 +6,7 @@ import com.bitheads.braincloud.client.StatusCodes;
 import com.bitheads.braincloud.client.BrainCloudClient;
 import com.bitheads.braincloud.client.BrainCloudWrapper;
 
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +22,9 @@ import static org.junit.Assert.*;
  */
 public class AuthenticationServiceTest extends TestFixtureNoAuth
 {
+
+    long mostRecentPacket = -1000000;
+    long secondMostRecentPacket = -1000000;
 
     @Test
     public void testAuthenticateAnonymous() throws Exception
@@ -163,5 +167,114 @@ public class AuthenticationServiceTest extends TestFixtureNoAuth
                 tr);
 
         tr.Run();
+    }
+    
+    @Test
+    public void testBadSig() throws Exception
+    {
+        //our problem is that users who refresh their app secret via the portal, the client would fail to read the response, and would retry infinitely.
+        //This threatens our servers, because huge numbers of errors related to bad signature show up, and infinitely retry to get out of this error.
+        //Instead of updating the signature via the portal, we will mimic a bad signature from the client.
+        Map<String, String> originalAppSecretMap = new HashMap<String, String>();
+        originalAppSecretMap.put(m_appId, m_secret);
+        originalAppSecretMap.put(m_childAppId, m_childSecret);
+        int numRepeatBadSigFailures = 0;
+    
+        // mess up app 
+        Map<String, String> updatedAppSecretMap = new HashMap<String, String>();;
+    
+        for (Map.Entry<String, String> entry : originalAppSecretMap.entrySet())
+        {
+            updatedAppSecretMap.put(entry.getKey(), entry.getValue() + "123");
+        }  
+        /////////////////////Phase 1
+            //first auth
+            TestResult tr1 = new TestResult(_wrapper);
+            _wrapper.getClient().getAuthenticationService().authenticateUniversal(getUser(Users.UserB).id, getUser(Users.UserB).password, true, tr1);
+            if(tr1.Run())
+            {
+               //Check the packet coming in and compare it to the last recevied packet. if they're both -1, we may be in a repeating scenario.
+                if(mostRecentPacket == -1000000 && secondMostRecentPacket == -1000000)
+                {
+                    mostRecentPacket = _wrapper.getClient().getRestClient().getLastReceivedPacketId();
+                }
+                else
+                {
+                    secondMostRecentPacket = mostRecentPacket;
+                    mostRecentPacket = _wrapper.getClient().getRestClient().getLastReceivedPacketId();
+                }
+        
+                //Is there the sign of a repeat?
+                if(mostRecentPacket == -1 && secondMostRecentPacket == -1)
+                {
+                    numRepeatBadSigFailures++;
+                }
+                //we shouldnt expect more than 2 times that most recent and second most recent are both bad sig errors for this test, else its repeating itself. 
+                if (numRepeatBadSigFailures > 2) throw new Exception("Repeating Bad sig errors");
+            }
+        
+            //check state
+            _wrapper.getClient().getPlayerStateService().readUserState(tr1);
+        
+        //////////////////////////Phase 2
+            TestResult tr3 = new TestResult(_wrapper);
+            _wrapper.getClient().initializeWithApps(m_serverUrl, m_appId, updatedAppSecretMap, m_appVersion);
+            _wrapper.getClient().getAuthenticationService().authenticateUniversal(getUser(Users.UserB).id, getUser(Users.UserB).password, true, tr3);
+            if(tr3.RunExpectFail(StatusCodes.FORBIDDEN, ReasonCodes.BAD_SIGNATURE))
+            {
+               //Check the packet coming in and compare it to the last recevied packet. if they're both -1, we may be in a repeating scenario.
+               if(mostRecentPacket == -1000000 && secondMostRecentPacket == -1000000)
+               {
+                   mostRecentPacket = _wrapper.getClient().getRestClient().getLastReceivedPacketId();
+               }
+               else
+               {
+                   secondMostRecentPacket = mostRecentPacket;
+                   mostRecentPacket = _wrapper.getClient().getRestClient().getLastReceivedPacketId();
+               }
+        
+                //Is there the sign of a repeat?
+                if(mostRecentPacket == -1 && secondMostRecentPacket == -1)
+                {
+                    numRepeatBadSigFailures++;
+                }
+                //we shouldnt expect more than 2 times that most recent and second most recent are both bad sig errors for this test, else its repeating itself. 
+                if (numRepeatBadSigFailures > 2) throw new Exception("Repeating Bad sig errors");
+            }
+        
+            //check state
+            _wrapper.getClient().getPlayerStateService().readUserState(tr3);
+        
+            //wait a while
+            Thread.sleep(5 * 1000);
+        
+            /////////////////////Phase 3
+            TestResult tr5 = new TestResult(_wrapper);
+            _wrapper.getClient().initializeWithApps(m_serverUrl, m_appId, originalAppSecretMap, m_appVersion);
+            _wrapper.getClient().getAuthenticationService().authenticateUniversal(getUser(Users.UserB).id, getUser(Users.UserB).password, true, tr5);
+            if(tr5.Run())
+            {
+                //Check the packet coming in and compare it to the last recevied packet. if they're both -1, we may be in a repeating scenario.
+                if(mostRecentPacket == -1000000 && secondMostRecentPacket == -1000000)
+                {
+                   mostRecentPacket = _wrapper.getClient().getRestClient().getLastReceivedPacketId();
+                }
+                else
+                {
+                   secondMostRecentPacket = mostRecentPacket;
+                   mostRecentPacket = _wrapper.getClient().getRestClient().getLastReceivedPacketId();
+                }
+        
+                //Is there the sign of a repeat?
+                if(mostRecentPacket == -1 && secondMostRecentPacket == -1)
+                {
+                    numRepeatBadSigFailures++;
+                }
+                //we shouldnt expect more than 2 times that most recent and second most recent are both bad sig errors for this test, else its repeating itself. 
+                if (numRepeatBadSigFailures > 2) throw new Exception("Repeating Bad sig errors");
+            }
+        
+        //check state
+        _wrapper.getClient().getPlayerStateService().readUserState(tr5);
     }
 }
