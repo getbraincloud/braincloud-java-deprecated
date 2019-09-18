860eab025f2c4959179e1d717ca54e449ee1a2fb
ruth0048 <31827673+ruth0048@users.noreply.github.com>
2019-09-18 12:13:56 -0400

    prep for 4.2 (#57)
    * adding register and deregister RTTPresence callback
    
    * Inconsistent wrapper and comms initialize function calls. Improper param placements cause problems with comms
    
    * Fixing parameters in certain calls
    
    * quick fix to chat test
    
    * Bug fix for overly aggressive rest client thread polling.
    
    * Re-addition of InitializeWithApps, also added overload
    
    *  adding a note to keep initializeWithApps in the java client tests
    
    * 4.0 Api calls, and test fix
    
    * fixing java social leaderbaord tests.
    
    * RTT service java
    
    * cancelLobby
    
    * Need to take a second look at this assert
    
    * guard for sessions and profile id save
    
    * Updating version for release
    
    * Bad sig fix and tests to java
    
    * GroupLeaderboard calls and tests
    
    * changing groupLeaderboard Config again
    
    * group calls and tests java
    
    * changing createGroup to createGroupWithSummaryData for consistency across clients
    
    * updating version for java
    
    * itemCatalog and UserInventory services and temp tests
    
    * Blockchain
    
    * UserItems, PlayerState, CustomEntity
    
    * Datastream, AsyncMatch, Identity
    
    * updatign version

--- TestBCClient-Android/brainCloud/src/main/java/com/bitheads/braincloud/client/BrainCloudWrapper.java
+++ TestBCClient-Android/brainCloud/src/main/java/com/bitheads/braincloud/client/BrainCloudWrapper.java
@@ -38,7 +38,10 @@ import com.bitheads.braincloud.services.ScriptService;
 import com.bitheads.braincloud.services.SocialLeaderboardService;
 import com.bitheads.braincloud.services.TimeService;
 import com.bitheads.braincloud.services.TournamentService;
+import com.bitheads.braincloud.services.CustomEntityService;
 import com.bitheads.braincloud.services.VirtualCurrencyService;
+import com.bitheads.braincloud.services.ItemCatalogService;
+import com.bitheads.braincloud.services.UserItemsService;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -738,5 +741,17 @@ public class BrainCloudWrapper implements IServerCallback {
 
     public TournamentService getTournamentService() {
         return _client.getTournamentService();
+    }
+
+    public CustomEntityService getCustomEntityService() {
+        return _client.getCustomEntityService();
+    }
+
+    public ItemCatalogService getItemCatalogService() {
+        return _client.getItemCatalogService();
+    }
+
+    public UserItemsService getUserItemsService() {
+        return _client.getUserItemsService();
     }
 }
