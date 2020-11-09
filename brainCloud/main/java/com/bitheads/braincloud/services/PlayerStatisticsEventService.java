eb6bcc761ce19e4165103ba837126462428352bd
Daivuk <daivuk@gmail.com>
2020-11-09 09:12:04 -0500

    Reverted some deprecated

--- brainCloud/main/java/com/bitheads/braincloud/services/PlayerStatisticsEventService.java
+++ brainCloud/main/java/com/bitheads/braincloud/services/PlayerStatisticsEventService.java
@@ -38,6 +38,8 @@ public class PlayerStatisticsEventService {
      * Service Operation - Trigger
      *
      * @see PlayerStatisticsService
+     * 
+     * @deprecated User triggerStatsEvent instead - removal September 1, 2021
      */
     public void triggerUserStatsEvent(String eventName, int eventMultiplier, IServerCallback callback) {
         try {
@@ -48,6 +50,49 @@ public class PlayerStatisticsEventService {
             ServerCall sc = new ServerCall(ServiceName.playerStatisticsEvent, ServiceOperation.TRIGGER, data, callback);
             _client.sendRequest(sc);
 
+        } catch (JSONException ignored) {
+        }
+    }
+
+    /**
+     * Trigger an event server side that will increase the user statistics.
+     * This may cause one or more awards to be sent back to the user -
+     * could be achievements, experience, etc. Achievements will be sent by this
+     * client library to the appropriate awards service (Apple Game Center, etc).
+     *
+     * This mechanism supercedes the PlayerStatisticsService API methods, since
+     * PlayerStatisticsService API method only update the raw statistics without
+     * triggering the rewards.
+     *
+     * Service Name - PlayerStatisticsEvent
+     * Service Operation - Trigger
+     *
+     * @see PlayerStatisticsService
+     */
+    public void triggerStatsEvent(String eventName, int eventMultiplier, IServerCallback callback) {
+        try {
+            JSONObject data = new JSONObject();
+            data.put(Parameter.eventName.name(), eventName);
+            data.put(Parameter.eventMultiplier.name(), eventMultiplier);
+
+            ServerCall sc = new ServerCall(ServiceName.playerStatisticsEvent, ServiceOperation.TRIGGER, data, callback);
+            _client.sendRequest(sc);
+
+        } catch (JSONException ignored) {
+        }
+    }
+
+    /**
+     * @deprecated User triggerStatsEvents instead - removal September 1, 2021
+     */
+    public void triggerUserStatsEvents(String jsonData, IServerCallback callback) {
+        try {
+            JSONObject data = new JSONObject();
+            JSONArray jsonArray = new JSONArray(jsonData);
+            data.put(Parameter.events.name(), jsonArray);
+
+            ServerCall sc = new ServerCall(ServiceName.playerStatisticsEvent, ServiceOperation.TRIGGER_MULTIPLE, data, callback);
+            _client.sendRequest(sc);
         } catch (JSONException ignored) {
         }
     }
@@ -71,7 +116,7 @@ public class PlayerStatisticsEventService {
      *     }
      *   ]
      */
-    public void triggerUserStatsEvents(String jsonData, IServerCallback callback) {
+    public void triggerStatsEvents(String jsonData, IServerCallback callback) {
         try {
             JSONObject data = new JSONObject();
             JSONArray jsonArray = new JSONArray(jsonData);
