2e1d64024fb86dfb1b2e4c6c9c4b797c88d3f26d
franco <82105876+francobithead@users.noreply.github.com>
2022-10-19 15:22:35 -0400

    Started BlockchainService Test, still WIP

---
+++ brainCloud/test/java/com/bitheads/braincloud/services/BlockchainServiceTest.java
@@ -0,0 +1,34 @@
+package com.bitheads.braincloud.services;
+
+import com.bitheads.braincloud.client.BrainCloudClient;
+import com.bitheads.braincloud.client.ReasonCodes;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class BlockchainServiceTest extends TestFixtureBase {
+    private String _defaultIntegrationId = "default";
+    private String _defaultContextJson = "{}";
+     
+    @Test
+    public void testGetBlockchainItems() throws Exception {
+        TestResult tr = new TestResult(_wrapper);
+        
+        _wrapper.getClient().getBrainCloudBlockchainService.GetBlockchainItems(_defaultIntegrationId,
+                _defaultContextJson, tr);
+
+        tr.Run();
+    }
+
+    @Test
+    public void testGetUniqs() throws Exception {
+        TestResult tr = new TestResult(_wrapper);
+
+        _wrapper.getClient().getBrainCloudBlockchainService.GetUniqs(_defaultIntegrationId, _defaultContextJson, tr);
+
+        tr.Run();
+    }
+    
+}
